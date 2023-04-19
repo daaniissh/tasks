77c733e93e7fae854e0cb4e7f13c076ac26962b7
Danish <pkdanish329@gmail.com>
2023-04-19 15:43:15 +0530

    buggy code task completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
+function App() {
+   const [count, setCount] = useState(0);
+
+   const onIncrement = () => {
+      if (count >= 0 && count < 10) {
+         setCount(prev=>prev+1);
+      }
+   };
+   const onDecrement = () => {
+      if (count > 0) {
+         setCount(prev=>prev-1);
+      }
+   };
+   return (
+      <div className="App">
+         <Counter count={count} onIncrement={onIncrement} onDecrement={onDecrement} />
+      </div>
+   );
+}
+
+export default App;
